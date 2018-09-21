9a6feb1af9f32381ab4879ea4902c85100d02d6d
AbhilashJN <abhilashjn107@gmail.com>
2018-09-21 11:53:43 +0530

    basic server boilerplate

---
+++ server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func pong(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		fmt.Fprintln(w, "pong")
+	}
+}
+
+func registration(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		fmt.Fprintln(w, "registration successful")
+	}
+}
+func getReceiverInfo(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		fmt.Fprintln(w, "dummy customer info")
+	}
+}
+func sendPayment(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		fmt.Fprintln(w, "payment successful")
+	}
+}
+
+func main() {
+	http.HandleFunc("/ping", pong)
+	http.HandleFunc("/registration", registration)
+	http.HandleFunc("/getReceiverInfo", getReceiverInfo)
+	http.HandleFunc("/sendPayment", sendPayment)
+	http.ListenAndServe("localhost:8080", nil)
+}

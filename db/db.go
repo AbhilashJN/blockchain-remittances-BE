8db312725fb19a14e117ef1ebbc42de2f547d21c
sreekar <sreekar339@gmail.com>
2018-09-29 09:14:47 +0530

    major refactoring

--- db/db.go
+++ db/db.go
@@ -1,178 +1,15 @@
 package db
 
 import (
-	"bytes"
-	"encoding/gob"
 	"errors"
-	"fmt"
 
+	"github.com/AbhilashJN/blockchain-remittances-BE/data"
+	"github.com/AbhilashJN/blockchain-remittances-BE/utils"
 	"github.com/boltdb/bolt"
 )
 
-// StellarSeedsOfBank is
-
-type data interface {
-	String()
-}
-
-//StellarSeedsOfBank implements data, contains
-type StellarSeedsOfBank struct {
-	SourceSeed, IssuerSeed, DistributorSeed string
-}
-
-//TransactionDetails contains
-type TransactionDetails struct {
-	From, To, TransactionID string
-	TransactionType         string // debit or credit
-	Amount                  float64
-}
-
-//CustomerBankAccountDetails implements data, contains
-type CustomerBankAccountDetails struct {
-	Name         string
-	Balance      float64
-	Transactions []TransactionDetails
-}
-
-// CustomerDetails implements data, contains
-type CustomerDetails struct {
-	CustomerName, BankName, BankAccountID string
-}
-
-func (sab *StellarSeedsOfBank) String() {
-	fmt.Printf("%+v \n", sab)
-}
-
-func (sab *CustomerBankAccountDetails) String() {
-	fmt.Printf("%+v \n", sab)
-}
-
-func (sab *CustomerDetails) String() {
-	fmt.Printf("%+v \n", sab)
-}
-
-//gobDecode returns
-// func decodeByteSlice(dBdata []byte, target data) (data, error) {
-// 	fmt.Printf("\n\n In decodeByteSlice, dBdata: %s \n\n", dBdata)
-// 	err := json.Unmarshal(dBdata, target)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("json.Unmarshal(dBdata, target) failed: %s", err.Error())
-// 	}
-
-// 	return target, nil
-// }
-
-func gobEncode(structValue interface{}) ([]byte, error) {
-	buf := new(bytes.Buffer)
-	enc := gob.NewEncoder(buf)
-	err := enc.Encode(structValue)
-	if err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
-}
-
-func gobDecode(data []byte, target data) (data, error) {
-	// fmt.Printf("In gobDecode: %s", data)
-	buf := bytes.NewBuffer(data)
-	dec := gob.NewDecoder(buf)
-	err := dec.Decode(target)
-	if err != nil {
-		return nil, err
-	}
-	return target, nil
-}
-
-// UpdateCustomerBankAccountBalence returns
-func UpdateCustomerBankAccountBalence(txDetails *TransactionDetails, bankName, customerAccountID string) (*CustomerBankAccountDetails, *CustomerBankAccountDetails, error) {
-	// Open the database.
-	db, err := bolt.Open(bankName+".db", 0666, nil)
-	if err != nil {
-		return nil, nil, err
-	}
-	defer db.Close()
-	bucketName := "AccountDetails"
-	bankPoolAccountID := bankName + "-POOL-ID"
-	// fmt.Printf("bankPoolAccountID : %q", bankPoolAccountID)
-
-	var updatedRecordOfCustomer *CustomerBankAccountDetails
-	var updatedRecordOfBank *CustomerBankAccountDetails
-
-	if err := db.Update(
-		func(tx *bolt.Tx) error {
-			bucket := tx.Bucket([]byte(bucketName))
-
-			accDetOfCustomer, err := gobDecode(bucket.Get([]byte(customerAccountID)), &CustomerBankAccountDetails{})
-			if err != nil {
-				return fmt.Errorf("gobDecode(bucket.Get([]byte(customerAccountID)), &CustomerBankAccountDetails{}) failed: %s", err.Error())
-			}
-
-			accDetOfBankPool, err := gobDecode(bucket.Get([]byte(bankPoolAccountID)), &CustomerBankAccountDetails{})
-			if err != nil {
-				return fmt.Errorf("gobDecode(bucket.Get([]byte(bankPoolAccountID)), &CustomerBankAccountDetails{}) failed: %s", err.Error())
-			}
-
-			customerAccountDetails, ok := accDetOfCustomer.(*CustomerBankAccountDetails)
-			if !ok {
-				return errors.New("Could not update Customer Bank Account Details : Type assertion failed")
-			}
-			bankPoolaccountDetails, ok := accDetOfBankPool.(*CustomerBankAccountDetails)
-			if !ok {
-				return errors.New("Could not update Customer Bank Account Details : Type assertion failed")
-			}
-
-			switch txDetails.TransactionType {
-			case "credit":
-				fmt.Printf("In credit case")
-				bankPoolaccountDetails.Balance -= txDetails.Amount
-				customerAccountDetails.Balance += txDetails.Amount
-				customerAccountDetails.Transactions = append(customerAccountDetails.Transactions, *txDetails)
-				bankPoolaccountDetails.Transactions = append(bankPoolaccountDetails.Transactions, TransactionDetails{
-					To: customerAccountID, TransactionType: "debit", Amount: txDetails.Amount, TransactionID: fmt.Sprintf("PoolToCustomerTx:CustomerAccId-%s", customerAccountID),
-				})
-				fmt.Printf("bankPoolaccountDetails: %+v", bankPoolaccountDetails)
-			case "debit":
-				bankPoolaccountDetails.Balance += txDetails.Amount
-				customerAccountDetails.Balance -= txDetails.Amount
-				customerAccountDetails.Transactions = append(customerAccountDetails.Transactions, *txDetails)
-				bankPoolaccountDetails.Transactions = append(bankPoolaccountDetails.Transactions, TransactionDetails{
-					From: customerAccountID, TransactionType: "credit", Amount: txDetails.Amount, TransactionID: fmt.Sprintf("CustomerToPoolTx::CustomerAccId-%s", customerAccountID),
-				})
-			default:
-				return errors.New("invalid updateType param passed. should be 'credit' or 'debit'")
-			}
-
-			customerAccountDetailsEncoded, err := gobEncode(customerAccountDetails)
-			if err != nil {
-				return fmt.Errorf("gobEncode(customerAccountDetails) failed: %s", err.Error())
-			}
-
-			bankPoolaccountDetailsEncoded, err := gobEncode(bankPoolaccountDetails)
-			if err != nil {
-				return fmt.Errorf("gobEncode(bankPoolaccountDetails) failed: %s", err.Error())
-			}
-
-			if err := bucket.Put([]byte(customerAccountID), customerAccountDetailsEncoded); err != nil {
-				return fmt.Errorf("bucket.Put([]byte(customerAccountID), customerAccountDetailsEncoded) failed: %s", err.Error())
-			}
-
-			if err := bucket.Put([]byte(bankPoolAccountID), bankPoolaccountDetailsEncoded); err != nil {
-				return fmt.Errorf("bucket.Put([]byte(bankPoolAccountID), bankPoolaccountDetailsEncoded) failed: %s", err.Error())
-			}
-
-			updatedRecordOfCustomer = customerAccountDetails // copy in another var to return the updated record to the caller
-			updatedRecordOfBank = bankPoolaccountDetails
-			return nil
-		},
-	); err != nil {
-		return nil, nil, err
-	}
-
-	return updatedRecordOfCustomer, updatedRecordOfBank, nil
-}
-
 // WriteCustomerBankAccountDetails returns
-func WriteCustomerBankAccountDetails(bankName string, customerBankAccountID string, customerBankAccountDetails *CustomerBankAccountDetails) error {
+func WriteCustomerBankAccountDetails(bankName string, customerBankAccountID string, customerBankAccountDetails *data.CustomerBankAccountDetails) error {
 	// Open the database.
 	db, err := bolt.Open(bankName+".db", 0666, nil)
 	if err != nil {
@@ -181,13 +18,12 @@ func WriteCustomerBankAccountDetails(bankName string, customerBankAccountID stri
 	defer db.Close()
 	bucketName := "AccountDetails"
 
-	// Execute several commands within a read-write transaction.
 	if err := db.Update(
 		func(tx *bolt.Tx) error {
 			bucket := tx.Bucket([]byte(bucketName))
 			key := []byte(customerBankAccountID)
 
-			encoded, err := gobEncode(customerBankAccountDetails)
+			encoded, err := utils.GobEncode(customerBankAccountDetails)
 			if err != nil {
 				return err
 			}
@@ -206,7 +42,7 @@ func WriteCustomerBankAccountDetails(bankName string, customerBankAccountID stri
 }
 
 // ReadCustomerBankAccountDetails returns
-func ReadCustomerBankAccountDetails(bankName, customerBankAccountID string) (*CustomerBankAccountDetails, error) {
+func ReadCustomerBankAccountDetails(bankName, customerBankAccountID string) (*data.CustomerBankAccountDetails, error) {
 	// Open the database.
 	db, err := bolt.Open(bankName+".db", 0666, nil)
 	if err != nil {
@@ -215,14 +51,14 @@ func ReadCustomerBankAccountDetails(bankName, customerBankAccountID string) (*Cu
 	defer db.Close()
 	bucketName := "AccountDetails"
 
-	var accountDetails data
-	// Read the value back from a separate read-only transaction.
+	var accountDetails data.Data
+
 	if err := db.View(
 		func(tx *bolt.Tx) error {
 			bucket := tx.Bucket([]byte(bucketName))
 			key := []byte(customerBankAccountID)
 
-			accountDetails, err = gobDecode(bucket.Get(key), &CustomerBankAccountDetails{})
+			accountDetails, err = utils.GobDecode(bucket.Get(key), &data.CustomerBankAccountDetails{})
 			if err != nil {
 				return err
 			}
@@ -232,7 +68,7 @@ func ReadCustomerBankAccountDetails(bankName, customerBankAccountID string) (*Cu
 	); err != nil {
 		return nil, err
 	}
-	customerAccountDetails, ok := accountDetails.(*CustomerBankAccountDetails)
+	customerAccountDetails, ok := accountDetails.(*data.CustomerBankAccountDetails)
 	if !ok {
 		return nil, errors.New("Could not read Customer Bank Account Details : Type assertion failed")
 	}
@@ -254,38 +90,7 @@ func CreateDBForBank(bankName string) error {
 				return err
 			}
 
-			// encoded, err := gobEncode(&CustomerBankAccountDetails{
-			// 	Balance: 10000000.0,
-			// 	Name:    bankName,
-			// })
-			// if err != nil {
-			// 	return err
-			// }
-
-			// if err := b.Put([]byte(bankName), encoded); err != nil {
-			// 	return err
-			// }
-
-			return nil
-		},
-	); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// CreateBankStellarSeedsDB returns
-func CreateBankStellarSeedsDB() error {
-	db, err := bolt.Open("BankStellarSeeds.db", 0600, nil)
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-
-	if err := db.Update(
-		func(tx *bolt.Tx) error {
-			_, err := tx.CreateBucketIfNotExists([]byte("StellarSeeds"))
+			_, err = tx.CreateBucket([]byte("StellarSeeds"))
 			if err != nil {
 				return err
 			}
@@ -300,8 +105,8 @@ func CreateBankStellarSeedsDB() error {
 }
 
 // WriteStellarSeedsForBank returns
-func WriteStellarSeedsForBank(bankName string, addresses *StellarSeedsOfBank) error {
-	db, err := bolt.Open("BankStellarSeeds.db", 0600, nil)
+func WriteStellarSeedsForBank(bankName string, seeds *data.StellarSeeds) error {
+	db, err := bolt.Open(bankName+".db", 0600, nil)
 	if err != nil {
 		return err
 	}
@@ -312,7 +117,7 @@ func WriteStellarSeedsForBank(bankName string, addresses *StellarSeedsOfBank) er
 			b := tx.Bucket([]byte("StellarSeeds"))
 			key := []byte(bankName)
 
-			encoded, err := gobEncode(addresses)
+			encoded, err := utils.GobEncode(seeds)
 			if err != nil {
 				return err
 			}
@@ -332,20 +137,20 @@ func WriteStellarSeedsForBank(bankName string, addresses *StellarSeedsOfBank) er
 }
 
 // ReadStellarSeedsOfBank returns
-func ReadStellarSeedsOfBank(bankName string) (*StellarSeedsOfBank, error) {
-	db, err := bolt.Open("BankStellarSeeds.db", 0600, nil)
+func ReadStellarSeedsOfBank(bankName string) (*data.StellarSeeds, error) {
+	db, err := bolt.Open(bankName+".db", 0600, nil)
 	if err != nil {
 		return nil, err
 	}
 	defer db.Close()
 
-	var dataVal data
+	var dataVal data.Data
 	if err := db.View(
 		func(tx *bolt.Tx) error {
 			b := tx.Bucket([]byte("StellarSeeds"))
 			key := []byte(bankName)
 
-			dataVal, err = gobDecode(b.Get(key), &StellarSeedsOfBank{})
+			dataVal, err = utils.GobDecode(b.Get(key), &data.StellarSeeds{})
 			if err != nil {
 				return err
 			}
@@ -356,16 +161,16 @@ func ReadStellarSeedsOfBank(bankName string) (*StellarSeedsOfBank, error) {
 		return nil, err
 	}
 
-	stellarAddress, ok := dataVal.(*StellarSeedsOfBank)
+	stellarAddress, ok := dataVal.(*data.StellarSeeds)
 	if !ok {
 		return nil, errors.New("Could not read Stellar Bank Address : Type assertion failed")
 	}
 	return stellarAddress, nil
 }
 
-// CreateCommonCustomersDB returns
-func CreateCommonCustomersDB() error {
-	db, err := bolt.Open("CommonCustomers.db", 0600, nil)
+// CreateCustomerPoolDB returns
+func CreateCustomerPoolDB() error {
+	db, err := bolt.Open("CustomerPool.db", 0600, nil)
 	if err != nil {
 		return err
 	}
@@ -386,21 +191,21 @@ func CreateCommonCustomersDB() error {
 	return nil
 }
 
-//ReadCustomerDetailsFromCommonCustomersDB returns
-func ReadCustomerDetailsFromCommonCustomersDB(phoneNumber string) (*CustomerDetails, error) {
-	db, err := bolt.Open("CommonCustomers.db", 0600, nil)
+//ReadCustomerDetailsFromCustomerPoolDB returns
+func ReadCustomerDetailsFromCustomerPoolDB(phoneNumber string) (*data.CustomerDetails, error) {
+	db, err := bolt.Open("CustomerPool.db", 0600, nil)
 	if err != nil {
 		return nil, err
 	}
 	defer db.Close()
 
-	var dataVal data
+	var dataVal data.Data
 	if err := db.View(
 		func(tx *bolt.Tx) error {
 			b := tx.Bucket([]byte("CustomerDetails"))
 			key := []byte(phoneNumber)
 
-			dataVal, err = gobDecode(b.Get(key), &CustomerDetails{})
+			dataVal, err = utils.GobDecode(b.Get(key), &data.CustomerDetails{})
 			if err != nil {
 				return err
 			}
@@ -411,7 +216,7 @@ func ReadCustomerDetailsFromCommonCustomersDB(phoneNumber string) (*CustomerDeta
 		return nil, err
 	}
 
-	cd, ok := dataVal.(*CustomerDetails)
+	cd, ok := dataVal.(*data.CustomerDetails)
 	if !ok {
 		return nil, errors.New("Could not read Customer Details: Type assertion failed")
 	}
@@ -419,9 +224,9 @@ func ReadCustomerDetailsFromCommonCustomersDB(phoneNumber string) (*CustomerDeta
 	return cd, nil
 }
 
-// WriteCustomerDetailsToCommonCustomersDB returns
-func WriteCustomerDetailsToCommonCustomersDB(phoneNumber string, customerDetails *CustomerDetails) error {
-	db, err := bolt.Open("CommonCustomers.db", 0600, nil)
+// WriteCustomerDetailsToCustomerPoolDB returns
+func WriteCustomerDetailsToCustomerPoolDB(phoneNumber string, customerDetails *data.CustomerDetails) error {
+	db, err := bolt.Open("CustomerPool.db", 0600, nil)
 	if err != nil {
 		return err
 	}
@@ -432,7 +237,7 @@ func WriteCustomerDetailsToCommonCustomersDB(phoneNumber string, customerDetails
 			b := tx.Bucket([]byte("CustomerDetails"))
 			key := []byte(phoneNumber)
 
-			encoded, err := gobEncode(customerDetails)
+			encoded, err := utils.GobEncode(customerDetails)
 			if err != nil {
 				return err
 			}
---
+++ setup/setup.go
@@ -0,0 +1,193 @@
+package setup
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/AbhilashJN/blockchain-remittances-BE/account"
+	"github.com/AbhilashJN/blockchain-remittances-BE/data"
+	"github.com/AbhilashJN/blockchain-remittances-BE/db"
+	"github.com/stellar/go/build"
+	"github.com/stellar/go/clients/horizon"
+	"github.com/stellar/go/keypair"
+)
+
+// createNewAccFromSourceAcc returns
+func createNewAccFromSourceAcc(sourceAcc *keypair.Full, initBalance string) (string, error) {
+	sourceAccSeed := sourceAcc.Seed()
+	newAccountKeyPair := account.MakePair()
+
+	tx, err := build.Transaction(
+		build.SourceAccount{AddressOrSeed: sourceAccSeed},
+		build.AutoSequence{SequenceProvider: horizon.DefaultTestNetClient},
+		build.TestNetwork,
+		build.CreateAccount(
+			build.Destination{AddressOrSeed: newAccountKeyPair.Address()},
+			build.NativeAmount{Amount: initBalance},
+		),
+	)
+
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+
+	txe, err := tx.Sign(sourceAccSeed)
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+
+	txeB64, err := txe.Base64()
+
+	if err != nil {
+		fmt.Println(err)
+		return "", err
+	}
+
+	_, err = horizon.DefaultTestNetClient.SubmitTransaction(txeB64)
+	if err != nil {
+		return "", err
+	}
+
+	return newAccountKeyPair.Seed(), nil
+}
+
+// CreateDistributionAccount returns
+func createDistributionAccount(sourceAccount *keypair.Full, initBalance string) (string, error) {
+	return createNewAccFromSourceAcc(sourceAccount, initBalance)
+}
+
+// CreateIssuingAccount returns
+func createIssuingAccount(sourceAccount *keypair.Full, initBalance string) (string, error) {
+	return createNewAccFromSourceAcc(sourceAccount, initBalance)
+}
+
+// CreateSourceAccount returns
+func createSourceAccount() (*keypair.Full, error) {
+	keyPair, err := account.CreateTestAccount()
+	if err != nil {
+		return nil, err
+	}
+	return keyPair, nil
+}
+
+// CreateTrust returns
+func createTrust(receiverSeed, senderSeed string, assetCode string) error {
+	issuer, err := keypair.Parse(senderSeed)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	receiver, err := keypair.Parse(receiverSeed)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tx, err := build.Transaction(
+		build.SourceAccount{AddressOrSeed: receiver.Address()},
+		build.AutoSequence{SequenceProvider: horizon.DefaultTestNetClient},
+		build.TestNetwork,
+		build.Trust(assetCode, issuer.Address()),
+	)
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	txe, err := tx.Sign(receiverSeed)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	txeB64, err := txe.Base64()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	_, err = horizon.DefaultTestNetClient.SubmitTransaction(txeB64)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+// IssueToDistribAccount returns
+func IssueToDistribAccount(issuerSeed, issuerAddress, receiverAddress, assetCode, amount string) error {
+
+	paymentTx, err := build.Transaction(
+		build.SourceAccount{AddressOrSeed: issuerAddress},
+		build.TestNetwork,
+		build.AutoSequence{SequenceProvider: horizon.DefaultTestNetClient},
+		build.Payment(
+			build.Destination{AddressOrSeed: receiverAddress},
+			build.CreditAmount{Code: assetCode, Issuer: issuerAddress, Amount: amount},
+		),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	paymentTxe, err := paymentTx.Sign(issuerSeed)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	paymentTxeB64, err := paymentTxe.Base64()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = horizon.DefaultTestNetClient.SubmitTransaction(paymentTxeB64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
+
+// OnboardBank returns
+func OnboardBank(bankName, assetCode, issuingAccMinBal, distributingAccMinBal string) error {
+	err := db.CreateDBForBank(bankName)
+	if err != nil {
+		return err
+	}
+
+	sourceAccKeyPair, err := createSourceAccount()
+	if err != nil {
+		return err
+	}
+
+	issuerSeed, err := createIssuingAccount(sourceAccKeyPair, issuingAccMinBal)
+	if err != nil {
+		return err
+	}
+
+	distributorSeed, err := createDistributionAccount(sourceAccKeyPair, distributingAccMinBal)
+	if err != nil {
+		return err
+	}
+
+	err = createTrust(distributorSeed, issuerSeed, assetCode)
+	if err != nil {
+		return err
+	}
+
+	err = db.WriteStellarSeedsForBank(bankName, &data.StellarSeeds{
+		Keys: data.Keys{
+			Source:      sourceAccKeyPair.Seed(),
+			Issuer:      issuerSeed,
+			Distributor: distributorSeed,
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
